899cce45afffa6a200a140213631016fb2813277
Joshua <twogoodgamer2@gmail.com>
2023-06-17 21:20:15 -0600

    fix: Added proper setup for installers

--- installers/fortnitesetup.sh
+++ installers/fortnitesetup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-FORTNITE_LINUX="v0.0.1"
+FORTNITE_LINUX="v1.0.0"
 PROTON_URL="https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton8-3/GE-Proton8-3.tar.gz"
 EPICGAMES_URL="https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi"
 DOTNET_URL="https://download.visualstudio.microsoft.com/download/pr/85473c45-8d91-48cb-ab41-86ec7abc1000/83cd0c82f0cde9a566bae4245ea5a65b/windowsdesktop-runtime-6.0.16-win-x64.exe"
-
 PROTON_BUILD="GE-Proton8-3"
 EPICGAMES_INSTALLER="EpicInstaller-15.5.0.msi"
 
@@ -22,7 +21,7 @@ cd ~/.fortnitelinux/
 printf "%s\n" "DOWNLOAD: Downloading Fortnite-Linux setup version: $FORTNITE_LINUX";
 
 rm -rf fortnite-linux || true
-wget https://github.com/pikdum/vortex-linux/releases/download/$VORTEX_LINUX/vortex-linux
+wget https://github.com/SirStig/fortnite-deck/releases/download/$FORTNITE_LINUX/fortnite-linux
 chmod +x fortnite-linux
 
 # set STEAM_RUNTIME_PATH to internal storage or sd card
@@ -36,13 +35,13 @@ else
     exit 1
 fi
 
-./epic-linux setConfig STEAM_RUNTIME_PATH $STEAM_RUNTIME_PATH
-./epic-linux downloadProton "$PROTON_URL"
-./epic-linux setProton "$PROTON_BUILD"
-./epic-linux downloadEpic "$EPICGAMES_URL"
-./epic-linux protonRunUrl "$DOTNET_URL" /q
-./epic-linux setupEpicDesktop
-./epic-linux installEpic "$EPICGAMES_INSTALLER"
+./fortnite-linux setConfig STEAM_RUNTIME_PATH $STEAM_RUNTIME_PATH
+./fortnite-linux downloadProton "$PROTON_URL"
+./fortnite-linux setProton "$PROTON_BUILD"
+./fortnite-linux downloadEpic "$EPICGAMES_URL"
+./fortnite-linux protonRunUrl "$DOTNET_URL" /q
+./fortnite-linux setupEpicDesktop
+./fortnite-linux installEpic "$EPICGAMES_INSTALLER"
 
 cd ~/.-linux/compatdata/pfx/dosdevices
 
@@ -56,13 +55,8 @@ fi
 
 update-desktop-database || true
 
-rm -f ~/Desktop/install-vortex.desktop
-ln -sf ~/.local/share/applications/vortex.desktop ~/Desktop/
-ln -sf ~/.pikdum/steam-deck-master/vortex/update-vortex-library.desktop ~/Desktop/
-ln -sf ~/.pikdum/steam-deck-master/vortex/use-script-extender-launchers.desktop ~/Desktop/
-ln -sf ~/.pikdum/steam-deck-master/vortex/swap-to-vanilla-launcher.desktop ~/Desktop/
-
-mkdir -p /run/media/mmcblk0p1/vortex-downloads || true
+rm -f ~/Desktop/epicgameslauncher.desktop
+ln -sf ~/.local/share/applications/epicgameslauncher.desktop ~/Desktop/
 
 printf "%s\n" "SUCCESS: Closing in 3..."
 sleep 3
